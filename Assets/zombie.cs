d6f228c2aff9a5ed2ec55f20d9ccf446199cdd50
Newbilius <newbilius@gmail.com>
2013-11-27 22:20:57 +0600

    Зомби. Ходит.

--- Assets/zombie.cs
+++ Assets/zombie.cs
@@ -2,42 +2,41 @@
 using System.Collections;
 using Utils;
 
-public class dave : MonoBehaviour {
+public class zombie : MonoBehaviour {
 
-	Utils.Directions direction=Directions.NONE;
+	Utils.Directions direction=Directions.LEFT;
 	Animator animator;
 	float speed=30f;
-	float MaxSpeed=2f;
+	float MaxSpeed=1f;
 	Vector3 tmpVector3;
 	private Transform down_bump;
-
+	
 	void Start () {
 		animator = GetComponent<Animator>();
-		down_bump = transform.Find("down_bumper");
 	}
-
+	
 	void FixedUpdate () {
 		switch (direction) {
 		case Directions.LEFT:
 			rigidbody2D.AddForce(-Vector2.right * speed);
 			animator.Play("move");
+
+			tmpVector3=transform.localScale;
+			tmpVector3.x=-1;
+			transform.localScale=tmpVector3;
 			break;
 		case Directions.RIGHT:
 			rigidbody2D.AddForce(Vector2.right * speed);
 			animator.Play("move");
+
+			tmpVector3=transform.localScale;
+			tmpVector3.x=1;
+			transform.localScale=tmpVector3;
 			break;
-		case Directions.NONE:
-			animator.Play("stand");
-		break;
 		};
-		bool grounded = Physics2D.Linecast(transform.position, down_bump.position, 1 << LayerMask.NameToLayer("World"));  
 		
-		if (grounded && Input.GetKey (KeyCode.UpArrow)) {
-			rigidbody2D.AddForce(new Vector2(0f, 350));
-		}
-
 	}
-
+	
 	void Update () {
 		if (rigidbody2D.velocity.x > MaxSpeed) {
 			Vector2 newVelocity = rigidbody2D.velocity;
@@ -49,20 +48,6 @@ public class dave : MonoBehaviour {
 			newVelocity.x = -MaxSpeed;
 			rigidbody2D.velocity = newVelocity;
 		}
-
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			tmpVector3=transform.localScale;
-			tmpVector3.x=-1;
-			transform.localScale=tmpVector3;
-						direction = Directions.LEFT;
-				} else
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			tmpVector3=transform.localScale;
-			tmpVector3.x=1;
-			transform.localScale=tmpVector3;
-						direction = Directions.RIGHT;
-				} else {
-						direction = Directions.NONE;
-				}
 	}
+
 }
